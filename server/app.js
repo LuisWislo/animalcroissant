4e9072e5ffc70958d388948e0ce974c6b55ce867
LuisWislo <luisfwilson1998@gmail.com>
2020-06-10 22:54:31 -0500

    removed server

--- server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
- 
-app.get('/api/test', (req, res) => {
-    res.send('Asami the sexuality bender')
-})
- 
-app.listen(3000, () => console.log('blog server running on port 3000!'))
